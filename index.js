da1566126eccfe396ee7d8caf2ffd425cca50929
Khanisic <62929300+Khanisic@users.noreply.github.com>
2021-02-14 00:05:47 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,60 @@
+async function Rates(callback) {
+    var ExchangeRatesDetails = await fetch('https://api.exchangeratesapi.io/latest');
+    ExchangeRatesDetails = await ExchangeRatesDetails.json();
+    callback(ExchangeRatesDetails);
+}
+
+const data = Rates( data =>{
+    console.log(data);
+})
+
+
+var CUR = ['CAD','INR','GBP','JPY','USD'];
+
+const valueCAD = document.getElementById('CAD');
+const valueINR = document.getElementById('INR');
+const valueGBP = document.getElementById('GBP');
+const valueJPY = document.getElementById('JPY');
+const valueUSD = document.getElementById('USD');
+// console.log(valueCAD);
+
+valueINR.oninput = function(){
+    var temp = valueINR.value;
+    Rates( data =>{
+        valueCAD.value = ( temp * data.rates.CAD / data.rates.INR ).toFixed(2);
+        valueGBP.value = (temp * data.rates.GBP / data.rates.INR).toFixed(2);
+        valueJPY.value = (temp * data.rates.JPY / data.rates.INR).toFixed(2);
+        valueUSD.value = (temp * data.rates.USD / data.rates.INR).toFixed(2);
+    });
+
+};
+valueGBP.oninput = function(){
+    var temp = valueGBP.value;
+    Rates( data =>{
+        valueINR.value = ( temp * data.rates.INR / data.rates.GBP ).toFixed(2);
+        valueCAD.value = (temp * data.rates.GBP / data.rates.GBP).toFixed(2);
+        valueJPY.value = (temp * data.rates.JPY / data.rates.CAD).toFixed(2);
+        valueUSD.value = (temp * data.rates.USD / data.rates.GBP).toFixed(2);
+    });
+
+};
+valueJPY.oninput = function(){
+    var temp = valueJPY.value;
+    Rates( data =>{
+        valueINR.value = ( temp * data.rates.INR / data.rates.JPY ).toFixed(2);
+        valueGBP.value = (temp * data.rates.GBP / data.rates.JPY).toFixed(2);
+        valueCAD.value = (temp * data.rates.JPY / data.rates.JPY).toFixed(2);
+        valueUSD.value = (temp * data.rates.USD / data.rates.JPY).toFixed(2);
+    });
+
+};
+valueUSD.oninput = function(){
+    var temp = valueUSD.value;
+    Rates( data =>{
+        valueINR.value = ( temp * data.rates.INR / data.rates.USD ).toFixed(2);
+        valueGBP.value = (temp * data.rates.GBP / data.rates.USD).toFixed(2);
+        valueJPY.value = (temp * data.rates.JPY / data.rates.USD).toFixed(2);
+        valueCAD.value = (temp * data.rates.USD / data.rates.USD).toFixed(2);
+    });
+
+};
